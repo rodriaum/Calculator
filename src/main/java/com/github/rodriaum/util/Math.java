113b030fc51bf2f988426bba8b5ca2ec67f1e040
Rodrigão <rodrigofxxx04@gmail.com>
2023-09-11 22:38:43 +0100

    Math (Math.java) calculation methods up

---
+++ src/main/java/com/github/rodriaum/util/Math.java
@@ -0,0 +1,20 @@
+package com.github.rodriaum.util;
+
+public class Math {
+
+    public static double addition(double a, double b) {
+        return a + b;
+    }
+
+    public static double subtraction(double a, double b) {
+        return a - b;
+    }
+
+    public static double multiplication(double a, double b) {
+        return a * b;
+    }
+
+    public static double division(double a, double b) {
+        return a / b;
+    }
+}
