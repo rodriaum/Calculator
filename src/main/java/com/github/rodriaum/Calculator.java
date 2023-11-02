325bf784eb27dbeea15f95d7fb7c7f2e3b86d4e8
Rodrigão <rodrigofxxx04@gmail.com>
2023-11-02 17:51:43 +0000

    Ciclo adicionado

--- src/main/java/com/github/rodriaum/Calculator.java
+++ src/main/java/com/github/rodriaum/Calculator.java
@@ -5,9 +5,10 @@ import com.github.rodriaum.util.Math;
 import java.util.Scanner;
 
 public class Calculator {
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
         System.out.println("-----------------");
         System.out.println("1. Adição");
         System.out.println("2. Subtração");
@@ -15,30 +16,42 @@ public class Calculator {
         System.out.println("4. Divisão");
         System.out.println("-----------------");
 
-        System.out.print("Escolha o número correspondente ao método de calculo: ");
+        System.out.println("Escolha o número correspondente ao método de calculo: ");
         int method = scanner.nextInt();
 
-        System.out.print("Dígite o primeiro número do calculo: ");
-        double num1 = scanner.nextDouble();
+        boolean hasMore = true;
 
-        System.out.print("Dígite o segundo número do calculo: ");
-        double num2 = scanner.nextDouble();
-        
-        switch (method) {
-            case 1:
-                System.out.println("Resultado: " + Math.addition(num1, num2));
-                break;
-            case 2:
-                System.out.println("Resultado: " + Math.subtraction(num1, num2));
-                break;
-            case 3:
-                System.out.println("Resultado: " + Math.multiplication(num1, num2));
-                break;
-            case 4:
-                System.out.println("Resultado: " + Math.division(num1, num2));
-                break;
-            default:
-                break;
+        while (hasMore) {
+            System.out.println("Dígite os números para calculo: ");
+            double values = scanner.nextDouble();
+
+            double amount = 0;
+
+            switch (method) {
+                case 1:
+                    amount = Math.addition(amount, values);
+                    break;
+                case 2:
+                    amount = Math.subtraction(amount, values);
+                    break;
+                case 3:
+                    amount =  Math.multiplication(amount, values);
+                    break;
+                case 4:
+                    amount = Math.division(amount, values);
+                    break;
+                default:
+                System.err.println("ERRO! Método de calculo ínvalido.");
+                    break;
+            }
+
+            System.out.println("Deseja adicionar mais números? (true ou false):");
+            hasMore = scanner.nextBoolean();
+
+            if (hasMore == false) 
+            System.out.println("Resultado: " + amount);
+
         }
+        scanner.close();
     }
 }
